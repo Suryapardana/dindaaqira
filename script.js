4c81b0db86003f527c8f820dc08fe80940df9136
Angga Surya <evcf1703@gmail.com>
2022-06-02 21:58:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,188 @@
-var _0x12de=['evening,','parentNode','ontouchmove','scale','DIV','Jangan\x20lupa\x20tidur\x20ya\x20cantik','getMinutes','Good\x20','now','heart','afternoon,','Jaga\x20kesehatan\x20ya\x20disana','Jangan\x20lupa\x20tidur\x20ya\x20cantik','Jangan\x20lupa\x20tidur\x20ya\x20cantik','setAttribute','insertBefore','ontouchend','random','sore','top','Jaga\x20kesehatan\x20ya\x20disana','querySelector','Jaga\x20kesehatan\x20ya\x20disana','pageY','Jaga\x20kesehatan\x20ya\x20disana','Jangan\x20lupa\x20makan\x20ya','Jangan\x20lupa\x20tidur\x20ya\x20cantik','innerHTML','offsetTop','Jaga\x20kesehatan\x20ya\x20disana','time','transform','bound','getElementById','ontouchstart','getHours','pagi','push','Jaga\x20kesehatan\x20ya\x20disana','classList','background','floor','offsetLeft','night,','sin','onmousedown','round','direction','Jangan\x20lupa\x20tidur\x20ya\x20cantik','getSeconds','Jaga\x20kesehatan\x20ya\x20disana','onmouseup','add','length','style','touches','splice','\x20Dinda','quo','Jangan\x20lupa\x20tidur\x20ya\x20cantik','body','left','board','Jaga\x20kesehatan\x20ya\x20disana','createElement'];(function(_0x1afc38,_0x12de41){var _0x5e7930=function(_0x20ecbf){while(--_0x20ecbf){_0x1afc38['push'](_0x1afc38['shift']());}};_0x5e7930(++_0x12de41);}(_0x12de,0xd5));var _0x5e79=function(_0x1afc38,_0x12de41){_0x1afc38=_0x1afc38-0x0;var _0x5e7930=_0x12de[_0x1afc38];return _0x5e7930;};let i=0x0;let name=_0x5e79('0x27');let gud=_0x5e79('0x36');let pagi=gud+'morning,'+name;let siang=gud+_0x5e79('0x39')+name;let malam=gud+_0x5e79('0x2f')+name;let malem=gud+_0x5e79('0x19')+name;function startTime(){var _0x1bf525=new Date();var _0x1cf455=document[_0x5e79('0x3')]('#jam');var _0x57db93=_0x1bf525[_0x5e79('0x11')]();var _0xe1b44=_0x1bf525[_0x5e79('0x35')]();var _0x3eb2a3=_0x1bf525[_0x5e79('0x1f')]();_0xe1b44=checkTime(_0xe1b44);_0x3eb2a3=checkTime(_0x3eb2a3);_0x1cf455['innerHTML']=_0x57db93+':'+_0xe1b44+':'+_0x3eb2a3;var _0x32c63b=setTimeout(startTime,0x1f4);}function checkTime(_0x1cd7fe){if(_0x1cd7fe<0xa){_0x1cd7fe='0'+_0x1cd7fe;};return _0x1cd7fe;}function randQuo(){var _0x5391e4=[_0x5e79('0x20'),_0x5e79('0x6'),_0x5e79('0x1e'),_0x5e79('0x3a'),_0x5e79('0xb'),_0x5e79('0x7'),_0x5e79('0x2'),_0x5e79('0x14'),_0x5e79('0x2d'),_0x5e79('0x3b'),_0x5e79('0x3c'),'Jangan\x20lupa\x20tidur\x20ya\x20cantik',_0x5e79('0x29'),_0x5e79('0x34'),_0x5e79('0x8'),_0x5e79('0x4')];document[_0x5e79('0xf')](_0x5e79('0x28'))[_0x5e79('0x9')]=_0x5391e4[Math[_0x5e79('0x17')](Math[_0x5e79('0x40')]()*_0x5391e4[_0x5e79('0x23')])];}function jamS(){var _0x511079=document[_0x5e79('0x3')]('#teks');var _0x31f714=document[_0x5e79('0xf')](_0x5e79('0x16'));var _0x55cebb=document['body'][_0x5e79('0x24')]['backgroundColor'];var _0x4680db=new Date();if(_0x4680db['getHours']()<0xc){_0x511079[_0x5e79('0x9')]=pagi;_0x55cebb=_0x31f714['classList'][_0x5e79('0x22')](_0x5e79('0x12'));}else if(_0x4680db[_0x5e79('0x11')]()>=0xc&&_0x4680db['getHours']()<=0xf){_0x511079[_0x5e79('0x9')]=siang;_0x55cebb=_0x31f714[_0x5e79('0x15')][_0x5e79('0x22')]('siang');}else if(_0x4680db[_0x5e79('0x11')]()>0xf&&_0x4680db[_0x5e79('0x11')]()<0x13){_0x511079[_0x5e79('0x9')]=siang;_0x55cebb=_0x31f714['classList'][_0x5e79('0x22')](_0x5e79('0x0'));}else if(_0x4680db[_0x5e79('0x11')]()>=0x13&&_0x4680db[_0x5e79('0x11')]()<=0x18){_0x511079[_0x5e79('0x9')]=malam;_0x55cebb=_0x31f714['classList'][_0x5e79('0x22')]('malam');}else{_0x55cebb=_0x31f714[_0x5e79('0x15')][_0x5e79('0x22')]('malam');}}var brd=document['createElement'](_0x5e79('0x33'));document[_0x5e79('0x2a')][_0x5e79('0x3e')](brd,document['getElementById'](_0x5e79('0x2c')));const duration=0xbb8;const speed=0.5;const cursorXOffset=0x0;const cursorYOffset=-0x5;var hearts=[];function generateHeart(_0x5ee5b1,_0xaac72d,_0x2a7b5c,_0x16acf4,_0x1bda2d){var _0x3a0747=document[_0x5e79('0x2e')]('DIV');_0x3a0747[_0x5e79('0x3d')]('class',_0x5e79('0x38'));brd['appendChild'](_0x3a0747);_0x3a0747[_0x5e79('0xc')]=duration;_0x3a0747['x']=_0x5ee5b1;_0x3a0747['y']=_0xaac72d;_0x3a0747[_0x5e79('0xe')]=_0x2a7b5c;_0x3a0747[_0x5e79('0x1d')]=_0x16acf4;_0x3a0747[_0x5e79('0x24')][_0x5e79('0x2b')]=_0x3a0747['x']+'px';_0x3a0747[_0x5e79('0x24')][_0x5e79('0x1')]=_0x3a0747['y']+'px';_0x3a0747[_0x5e79('0x32')]=_0x1bda2d;_0x3a0747[_0x5e79('0x24')][_0x5e79('0xd')]='scale('+_0x1bda2d+','+_0x1bda2d+')';if(hearts==null)hearts=[];hearts[_0x5e79('0x13')](_0x3a0747);return _0x3a0747;}var down=![];var event=null;document[_0x5e79('0x1b')]=function(_0x2a86f7){down=!![];event=_0x2a86f7;};document[_0x5e79('0x21')]=function(_0x480add){down=![];};document['onmousemove']=function(_0x1dce19){event=_0x1dce19;};document[_0x5e79('0x10')]=function(_0x21834b){down=!![];event=_0x21834b[_0x5e79('0x25')][0x0];};document[_0x5e79('0x3f')]=function(_0x17a460){down=![];};document[_0x5e79('0x31')]=function(_0xc4c05e){event=_0xc4c05e[_0x5e79('0x25')][0x0];};var before=Date['now']();var id=setInterval(frame,0x5);var gr=setInterval(check,0x64);function frame(){var _0x35d854=Date[_0x5e79('0x37')]();var _0x46f75b=_0x35d854-before;before=_0x35d854;for(i in hearts){var _0x530341=hearts[i];_0x530341[_0x5e79('0xc')]-=_0x46f75b;if(_0x530341[_0x5e79('0xc')]>0x0){_0x530341['y']-=speed;_0x530341[_0x5e79('0x24')][_0x5e79('0x1')]=_0x530341['y']+'px';_0x530341[_0x5e79('0x24')]['left']=_0x530341['x']+_0x530341[_0x5e79('0x1d')]*_0x530341[_0x5e79('0xe')]*Math[_0x5e79('0x1a')](_0x530341['y']*_0x530341[_0x5e79('0x32')]/0x1e)/_0x530341['y']*0xc8+'px';}else{_0x530341[_0x5e79('0x30')]['removeChild'](_0x530341);hearts[_0x5e79('0x26')](i,0x1);}}}function check(){if(down){var _0x5e7601=0x1-Math[_0x5e79('0x1c')](Math[_0x5e79('0x40')]())*0x2;var _0x49f3a9=Math[_0x5e79('0x40')]()*Math[_0x5e79('0x40')]()*0.8+0.2;var _0x4ae6f5=0x1e+Math[_0x5e79('0x40')]()*0x14;generateHeart(event['pageX']-brd[_0x5e79('0x18')]+cursorXOffset,event[_0x5e79('0x5')]-brd[_0x5e79('0xa')]+cursorYOffset,_0x4ae6f5,_0x5e7601,_0x49f3a9);}}
+let TIME_LIMIT = 60 
+  
+const quotesList = [
+  'Jika saatnya tiba, sedih akan menjadi tawa, perih akan menjadi cerita, kenangan akan menjadi guru, rindu akan menjadi temu, kau dan aku akan menjadi kita. - Fiersa Besari',
+  'Cinta selalu saja misterius. Jangan diburu-buru, atau kau akan merusak jalan ceritanya sendiri. - Tere Liye',
+  'Rindu tidak menuntut banyak hal. Ia hanya mengharapkan kabar dan pertemuan. - Alam Arifin',
+  'Yang patah tumbuh, yang hilang berganti. - Haikal Art',
+  'Beberapa rindu memang harus sembunyi-sembunyi. Bukan untuk disampaikan, hanya untuk dikirimkan lewat doa. - Fiersa Besari',
+  'Hujan dan kamu adalah rindu. Kita akan menikmatinya dalam senja-senja beranjak pulang. Dalam rasa sayang yang tak akan pernah hilang. Bahkan saat hujan telah berhenti. - Boy Candra',
+  'Saat lantunan rindu adalah alasan setiap pertemuan. - Wira Nagara',
+  'Sungguh rindu membuat kebodohan terasa begitu cerdas. - Wira Nagara',
+  'Kesalahanku, menjadikanmu alasan segala rindu. - Wira Nagara',
+  'Jika panas, keringkan lukamu. Jika hujan, nikmati rindu. Jika gelap, biarkan harapan menuntumu. Mentari akan selalu terbit, juga senyumanmu. - Fiersa Besari',
+  'Mungkin seseorang masih tak tahu lirih perih dalam rintik rindu ini. Selamat malam, kamu yang berlalu dalam gerimis. - Helvy Tiana Rosa',
+  'Jika nanti, semesta bercanda dan mempertemukan kita lagi. Segeralah menghindar, sebab bagiku kamu tidak lagi sesuatu yang menarik meski rindu tak sepenuhnya memudar. - Boy Candra',
+  'Kalau sampai rindu, aku tidak akan bilang-bilang, aku akan datang. Tidak ada yang berat, selama hati kita masih erat. - Fiersa Besari',
+  'Jika kau percaya hujan adalah satu persen air, sembilan puluh sembilan persen kenangan, ya silakan. Kalau aku percaya hujan seratus persen adalah rindu, itu urusanku. - Boy Candra',
+  'Lucu, kita membentuk pola pikir anak kecil agar tumbuh menjadi seperti kita. Padahal, diam-diam kita rindu menjadi anak kecil lagi. - Fiersa Besari'
+]
+
+let randomQuotes = quotesList[Math.floor(Math.random()*quotesList.length)]
+let randomQuotes1 = quotesList[Math.floor(Math.random()*quotesList.length * 1)]
+let randomQuotes2 = quotesList[Math.floor(Math.random()*quotesList.length * 2)]
+
+
+const quotes_array = [
+  `${randomQuotes}`, `${randomQuotes1}`, `${randomQuotes2}`
+]
+
+
+let timer_text = document.querySelector('.curr_time') 
+let accuracy_text = document.querySelector('.curr_accuracy') 
+let error_text = document.querySelector('.curr_errors') 
+let cpm_text = document.querySelector('.curr_cpm') 
+let wpm_text = document.querySelector('.curr_wpm') 
+let quote_text = document.querySelector('.quote') 
+let input_area = document.querySelector('.input_area') 
+let restart_btn = document.querySelector('.restart_btn') 
+let cpm_group = document.querySelector('.cpm') 
+let wpm_group = document.querySelector('.wpm') 
+let error_group = document.querySelector('.errors') 
+let accuracy_group = document.querySelector('.accuracy') 
+  
+let timeLeft = TIME_LIMIT 
+let timeElapsed = 0 
+let total_errors = 0 
+let errors = 0 
+let accuracy = 0 
+let characterTyped = 0 
+let current_quote = '' 
+let quoteNo = 0 
+let timer = null
+
+function showWord(words) {
+  // Generate random array index
+  const randIndex = Math.floor(Math.random() * words.length)
+  // Output random word
+  currentWord.innerHTML = words[randIndex]
+}
+
+function updateQuote() { 
+  quote_text.textContent = null 
+  current_quote = quotes_array[quoteNo] 
+
+    current_quote.split('').forEach(char => { 
+    const charSpan = document.createElement('span') 
+    charSpan.innerText = char 
+    quote_text.appendChild(charSpan) 
+  }) 
+ 
+  
+  if (quoteNo < quotes_array.length - 1) {
+    quoteNo++
+  } else {
+    quoteNo = 0
+  }
+}
+
+function processCurrentText() { 
+  
+  curr_input = input_area.value 
+  curr_input_array = curr_input.split('') 
+  
+   
+  characterTyped++ 
+  
+  errors = 0 
+  
+  quoteSpanArray = quote_text.querySelectorAll('span') 
+  quoteSpanArray.forEach((char, index) => { 
+    let typedChar = curr_input_array[index] 
+  
+      if (typedChar == null) { 
+      char.classList.remove('correct_char') 
+      char.classList.remove('incorrect_char') 
+     
+    } else if (typedChar === char.innerText) { 
+      char.classList.add('correct_char') 
+      char.classList.remove('incorrect_char') 
+  
+    } else { 
+      char.classList.add('incorrect_char') 
+      char.classList.remove('correct_char') 
+  
+        errors++ 
+    } 
+  }) 
+  
+   
+  error_text.textContent = total_errors + errors 
+  
+   
+  let correctCharacters = (characterTyped - (total_errors + errors)) 
+  let accuracyVal = ((correctCharacters / characterTyped) * 100) 
+  accuracy_text.textContent = Math.round(accuracyVal) 
+  
+  
+  if (curr_input.length == current_quote.length) { 
+    updateQuote() 
+  
+    total_errors += errors 
+   
+    input_area.value = '' 
+  } 
+}
+
+
+function startGame() {
+
+  resetValues()
+  updateQuote()
+
+  timer = setInterval(updateTimer, 1000)
+}
+
+function resetValues() {
+  timeLeft = TIME_LIMIT
+  timeElapsed = 0
+  errors = 0
+  total_errors = 0
+  accuracy = 0
+  characterTyped = 0
+  quoteNo = 0
+  input_area.disabled = false
+
+  input_area.value = ''
+  quote_text.textContent = 'Click on the area below to start the game.'
+  accuracy_text.textContent = 100
+  timer_text.textContent = timeLeft + 's'
+  error_text.textContent = 0
+  restart_btn.style.display = 'none' 
+  cpm_group.style.display = 'none' 
+  wpm_group.style.display = 'none'
+}
+
+function updateTimer() {
+  if (timeLeft > 0) {
+
+    timeLeft--
+
+    timeElapsed++
+
+    timer_text.textContent = timeLeft + 's'
+  }
+  else {
+
+    finishGame()
+  }
+}
+
+function finishGame() {
+
+  clearInterval(timer)
+
+  input_area.disabled = true
+
+  quote_text.textContent = 'Click on restart to start a new game.'
+
+  restart_btn.style.display = 'block'
+
+  cpm = Math.round(((characterTyped / timeElapsed) * 60))
+  wpm = Math.round((((characterTyped / 5) / timeElapsed) * 60))
+
+  cpm_text.textContent = cpm
+  wpm_text.textContent = wpm
+  cpm_group.style.display = 'block' 
+  wpm_group.style.display = 'block'
+}
